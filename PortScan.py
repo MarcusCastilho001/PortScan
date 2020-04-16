0f965538bd69b7912f9b2a20f867cde5926c5c20
root@root <61066188+MarcusCastilho001@users.noreply.github.com>
2020-04-16 20:26:31 -0300

    Add files via upload

--- PortScan.py
+++ PortScan.py
@@ -1,11 +1,13 @@
+
 #By: Marcus Castilho (root@root)
 #Date: 15/04/2020
+#Atualização: 16/04/2020 - 16:46
 
 
 import socket
 from time import sleep
 
-print ('''
+print ('''\033[01;031m
 (  ____ )(  ___  )(  ____ )\__   __/(  ____ \(  ____ \(  ___  )( (    /|
 | (    )|| (   ) || (    )|   ) (   | (    \/| (    \/| (   ) ||  \  ( |
 | (____)|| |   | || (____)|   | |   | (_____ | |      | (___) ||   \ | |
@@ -13,13 +15,13 @@ print ('''
 | (      | |   | || (\ (      | |         ) || |      | (   ) || | \   |
 | )      | (___) || ) \ \__   | |   /\____) || (____/\| )   ( || )  \  |
 |/       (_______)|/   \__/   )_(   \_______)(_______/|/     \||/    )_)
-''')
+\033[m''')
 print("                                By: Marcus Castilho (root@root)")
-print("						[versão 2.0]")
+print("						[versão 2.1]")
 print()
 sleep(2)
 
-dns = input("Dominio (ex: google.com ou IP): ")
+dns = input("Dominio (ex: google.com ou IP): ").lower()
 porta = int(input("Porta (ex: 80): "))
 
 
@@ -28,6 +30,6 @@ cliente.settimeout(0.1)
 codigo = cliente.connect_ex ((dns, porta))
 
 if codigo == 0:
-   print (porta, "OPEN")
+    print (porta, "OPEN")
 else:
    print (porta, "Closed")
